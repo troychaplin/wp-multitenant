b02364e6d5f0d842f1587958b3775aa54a0a5884
Troy Chaplin <troy.chaplin@gmail.com>
2019-06-25 14:03:28 -0400

    Updated the root config function

--- app/wp-config.php
+++ app/wp-config.php
@@ -1,58 +1,14 @@
 <?php
-/**
- * Include autoload
- *
- * @since WP Multi Tenant
- * @package WordPress Config
- * @version 1.0
- */
+echo 'root config<br>';
 
-require_once dirname( __DIR__ ) . '/vendor/autoload.php';
-
-
-/**
- * Load .env file
- *
- * @since WP Multi Tenant
- * @package WordPress Config
- * @version 1.0
- */
-
-$dotenv = Dotenv\Dotenv::create( __DIR__ );
-$dotenv->load();
-$dotenv->required( [ 'ENV_BASEPATH', 'ENV_PUBLICPATH', 'WP_ROOT_PATH', 'WP_STABLE_PATH', 'WP_ASSETS_PATH', 'DB_USER', 'DB_PASSWORD', 'DB_HOST' ] );
-
-/**
- * Include wp-config file from individual install
- *
- * @since WP Multi Tenant
- * @package WordPress Config
- * @version 1.0
- * @see https://gist.github.com/markjaquith/6214749 - original idea that functions without env vars
- *
- * TODO: make sure this works with wp-cli
- */
-
-$url_slugs = explode( '/', $_SERVER['REQUEST_URI'] );
-
-while ( count( $url_slugs ) > 0 ) {
-	$wpconfig_path = getenv( 'ENV_PUBLICPATH' ) . implode( '/', $url_slugs ) . '/wp-config.php';
-
-	if ( file_exists( $wpconfig_path ) ) {
-		include $wpconfig_path;
+$install_path       = str_replace( $_SERVER['DOCUMENT_ROOT'], '', dirname( $_SERVER['SCRIPT_FILENAME'] ) );
+$install_path_parts = explode( '/', $install_path );
+while ( count( $install_path_parts ) > 0 ) {
+	$install_path = $_SERVER['DOCUMENT_ROOT'] . implode( '/', $install_path_parts ) . '/wp-config.php';
+	if ( file_exists( $install_path ) ) {
+		include $install_path;
 		break;
 	} else {
-		array_pop( $url_slugs );
+		array_pop( $install_path_parts );
 	}
 }
-
-
-/**
- * Include global constants
- *
- * @since WP Multi Tenant
- * @package WordPress Config
- * @version 1.0
- */
-
-require_once getenv( 'WP_APP_PATH' ) . '/wp-constants.php';
