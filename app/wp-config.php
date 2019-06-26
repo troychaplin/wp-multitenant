f66128dcedf26e731d263ec917b7f230d2793771
Troy Chaplin <troy.chaplin@gmail.com>
2019-06-26 13:59:57 -0400

    Fixed warning in main wp-config.php

--- app/wp-config.php
+++ app/wp-config.php
@@ -10,8 +10,9 @@
  */
 $install_path       = str_replace( $_SERVER['DOCUMENT_ROOT'], '', dirname( $_SERVER['SCRIPT_FILENAME'] ) );
 $install_path_parts = explode( '/', $install_path );
+$install_path_count = count( $install_path_parts );
 
-while ( count( $install_path_parts ) > 0 ) {
+while ( $install_path_count > 0 ) {
 	$install_path = $_SERVER['DOCUMENT_ROOT'] . implode( '/', $install_path_parts ) . '/wp-config.php';
 
 	if ( file_exists( $install_path ) ) {
