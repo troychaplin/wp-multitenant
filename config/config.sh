b20e74f44812a4a925a67f5f92c5e17b8318af09
Troy Chaplin <troy.chaplin@gmail.com>
2019-07-12 22:30:57 -0400

    Adding config bash script

---
+++ config/config.sh
@@ -0,0 +1,5 @@
+## Redis Object Cache
+cp assets/mu-plugins/redis-cache/includes/object-cache.php assets/drop-ins
+
+## Redis Page Cache
+cp assets/mu-plugins/pj-page-cache-red/advanced-cache.php assets/drop-ins
