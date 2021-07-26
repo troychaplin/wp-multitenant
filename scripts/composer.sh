0a8a8968d5f70eb51a24ae62179273de943a1eee
Troy Chaplin <troy.chaplin@gmail.com>
2021-07-26 08:11:36 -0300

    Updated setup

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -1,5 +1,7 @@
 ## Bedrock Autoloader for mu-plugins
-cp _install-files/bedrock-autoloader.php assets/mu-plugins
+cp _install-files/_wp-bedrock.php assets/mu-plugins/bedrock-autoloader.php
+cp _install-files/_wp-silencer.php assets/plugins/index.php
+cp _install-files/_wp-silencer.php assets/themes/index.php
 
 ## Redis Object Cache
 cp assets/mu-plugins/redis-cache/includes/object-cache.php assets/drop-ins
